938fc2f9d86c8bed45a3eb89ddce516cd86b2a86
Aaliyah <diegokim@YARAs-MacBook-Air.local>
2019-09-17 14:39:02 -0400

    Add animation

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Doctor.create([{name:"Dr.paul", occupation:"Child Therapist", number:"203-747-0747",location:" 404 Rails st,Stamford,CT 06902"},{name:"Dr.Rodriguez",occupation:"Psychiatrist",number:"203-880-3098",location:"333 Elm ave,Stamford,CT 06902"},{name:"Dr.Ramierez",occupation:"Therapist",number:"475-098-8889",location:"60 Purp st,New haven,CT 06501"},{name:"Dr.Cappelli",occupation:"Child Therapist",number:"203-555-8034",location:"234 Summer st,Stamford,CT 06902"}])
+Doctor.create([{name:"Dr.paul", occupation:"Child Therapist", number:"203-747-0747",location:" 404 Rails st,Stamford,CT 06902"},{name:"Dr.Rodriguez",occupation:"Psychiatrist",number:"203-880-3098",location:"333 Elm ave,Stamford,CT 06902"},{name:"Dr.Ramierez",occupation:"Therapist",number:"475-098-8889",location:"60 Purp st,New haven,CT 06501"},{name:"Dr.Cappelli",occupation:"Child Therapist",number:"203-555-8034",location:"234 Summer st,Stamford,CT 06902"}])
